519b2a1f6aac2a5f918da57f8d456a932e401ade
Bogdan <skopthe6@gmail.com>
2025-06-04 10:14:36 +0200

    init: base vue 3 + tailwindcss project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main class="h-screen w-screen bg-secondary">
+    <RouterView />
+  </main>
+</template>
